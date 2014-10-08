e5b3400b2a8228f5ee2872171e90f7b279588b9a
blmille1 <brandon.miller@llesiant.com>
2014-10-08 09:19:20 -0500

    Added fail test (it always fails :)

---
+++ src/test/HelloCI.Tests/Tests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloCI.Tests {
+    [TestFixture]
+    public class Tests {
+        [Test]
+        public void FailTest() {
+            Assert.AreEqual(1,2);
+        }
+    }
+}
